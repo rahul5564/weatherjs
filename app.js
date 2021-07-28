287232480e6d938a6f758a45336d06c6532523d6
rahul <sraghavendra5564@gmail.com>
2021-07-28 21:03:23 +0530

    upadted

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 //city value
-var city = document.getElementById('inputLong').value;
+
 //submit option
 var submit = document.getElementById('submit').addEventListener('click',click);
 //output 
 var output = document.getElementById('info');
 
  async function click(){
+    var city = document.getElementById('inputLong').value;
         var api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c51b2a8944880ed480b471de869f3619`);
         var resp = await api.json();
-        console.log(resp)
+ 
         output.innerHTML = 
     `
         <p>City : ${resp.name}</p>
-    
+        <p>Temperature : ${resp.main.temp}</p>
         <p>Humidity : ${resp.main.humidity}</p>
         <p>Sealavel : ${resp.main.sea_level}</p>
         <p>Windspeed : ${resp.wind.speed}</p>
         <p>Country : ${resp.sys.country}</p>
     ` 
 }
+console.log(resp)
 
-    // <p>Temperature : ${resp.}</p>
+    
 
