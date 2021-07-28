6dd692506134612032738889bf96116ef1ba3589
rahul <sraghavendra5564@gmail.com>
2021-07-28 21:56:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ var submit = document.getElementById('submit').addEventListener('click',click);
 //output 
 var output = document.getElementById('info');
 
+
  async function click(){
-    var city = document.getElementById('inputLong');
-        var api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c51b2a8944880ed480b471de869f3619&units=metric`);
+        var city = document.getElementById('inputLong');
+        var api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=c51b2a8944880ed480b471de869f3619&units=metric`);
         var resp = await api.json();
- 
         output.innerHTML = 
     `
         <p>City : ${resp.name}</p>
@@ -17,14 +17,15 @@ var output = document.getElementById('info');
         <p>Humidity : ${resp.main.humidity}</p>
         <p>Sealavel : ${resp.main.sea_level}</p>
         <p>Windspeed : ${resp.wind.speed}</p>
+        <p>Latitude : ${resp.coord.lat}</p>
+        <p>Longitude : ${resp.coord.lon}</p>
         <p>Country : ${resp.sys.country}</p>
     ` 
-    city.addEventListener('keyup',(e) => {
-        var value = e.target.value;
-        console.log(value)
+    city.addEventListener('keyup', (e) => {
+        var uservalue = e.target.value;
+        if(uservalue === ""){
+            output.innerHTML = '';
+        }
     })
-}
-
-
     
-
+}
