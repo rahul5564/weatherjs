406ba8d07156357a17da32d047316a6bc187f43d
rahul <sraghavendra5564@gmail.com>
2021-07-28 21:55:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var submit = document.getElementById('submit').addEventListener('click',click);
 var output = document.getElementById('info');
 
  async function click(){
-    var city = document.getElementById('inputLong').value;
+    var city = document.getElementById('inputLong');
         var api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c51b2a8944880ed480b471de869f3619&units=metric`);
         var resp = await api.json();
  
@@ -19,6 +19,10 @@ var output = document.getElementById('info');
         <p>Windspeed : ${resp.wind.speed}</p>
         <p>Country : ${resp.sys.country}</p>
     ` 
+    city.addEventListener('keyup',(e) => {
+        var value = e.target.value;
+        console.log(value)
+    })
 }
 
 
