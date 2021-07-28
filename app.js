aa08458345b42eed8b38b99dc1bbfafab7774f91
rahul <sraghavendra5564@gmail.com>
2021-07-28 16:22:47 +0530

    set

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
-//lat value
-var lat = document.getElementById('inputLat').value;
-//long value
-var long = document.getElementById('inputLong').value;
+var ui = new UI;
+//city value
+var city = document.getElementById('inputLong').value;
 //submit option
 var submit = document.getElementById('submit').addEventListener('click',click);
 //output 
 var output = document.getElementById('info');
+ui.clearprofile()
 
-async function click(){
-    var latvalue = parseInt(lat);
-    var longvalue = parseInt(long);
-    var apikey = "ca0d4a5f74122736dd0893d9beeb805c";
-    var api = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latvalue}&lon=${longvalue}&appid=${apikey}`);
-    var resp = await api.json();
-    output.innerHTML = 
-`
-    <p>City : ${resp.name}</p>
-    <p>Weather : ${resp.weather[0].description}</p>
-    <p>Humidity : ${resp.main.humidity}</p>
-    <p>Sealavel : ${resp.main.sea_level}</p>
-    <p>Windspeed : ${resp.wind.speed}</p>
-    <p>Country : ${resp.sys.country}</p>
-`
-        
+
+ async function click(){
+        var api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c51b2a8944880ed480b471de869f3619&units=metric`);
+        var resp = await api.json();
+        output.innerHTML = 
+    `
+        <p>City : ${resp.name}</p>
+        <p>Temperature : ${resp.main.temp}</p>
+        <p>Humidity : ${resp.main.humidity}</p>
+        <p>Sealavel : ${resp.main.sea_level}</p>
+        <p>Windspeed : ${resp.wind.speed}</p>
+        <p>Country : ${resp.sys.country}</p>
+    ` 
 }
-
 
 
 
